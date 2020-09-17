323c7f535a904000ae0fdf419b55bdd554ba3790
Michael Ring <ringmrr@gmail.com>
2020-09-16 20:52:18 -0700

    Lab 4 Submission

--- Lab 4/Calculator/Calculator_v2.cpp
+++ Lab 4/Calculator/Calculator_v2.cpp
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------------------------------------
-FILENAME:           Calculator_v1.cpp
+FILENAME:           Calculator_v2.cpp
 DESCRIPTION:        Beginning version of Project 1 (Calculator)
 COMPILER:           MinGW C++ compiler
 NOTES:              
@@ -7,16 +7,14 @@ NOTES:
 MODIFICATION HISTORY:
 Author              Date            Version
 ---------------     ----------      ---------------
-Michael Ring        2020-09-14      0.1 - Created files, copied headers
+Michael Ring        2020-09-14      1.0 - Copied Calculator_v1, adjusted name and file header
                                             >> Source code copied from:
                                                 > Add_2_Numbers.gif on Canvas
                                                 > Get_Choice_v(1-6).gifs on Canvas
-Michael Ring        2020-09-14      0.2 - Created functions from old programs
-Michael Ring        2020-09-14      0.3 - Added function prototypes and successfully compiled
-Michael Ring        2020-09-16      0.4 - Added 'q' selection to GetChoice()
-                                            > q is now recognized as correct input, but like
-                                            > b and c, it does nothing but print a message.
-Michael Ring        2020-09-16      1.0 - Added q to menu message, added more comments
+                                                > Program 6-17 from Textbook pg. 342
+Michael Ring        2020-09-16      1.1 - Added bool flag 'goodbye' which allows loop to be closed
+                                                > case 'q' now sets 'goodbye' to true and quits
+Michael Ring        2020-09-16      2.0 - Added new function getXandY for modularity
 --------------------------------------------------------------------------------------------------*/
 
 // Required preprocessor directives
@@ -28,11 +26,15 @@ Michael Ring        2020-09-16      1.0 - Added q to menu message, added more co
 using namespace std;
 
 // Function prototypes
-char GetChoice();       
+char GetChoice();
+void getXandY();      
 void Add2Numbers();
 
-// Declared global constants
+// Global constants
 const string PROGRAMMER_NAME = "Michael Ring";
+
+// Global variables
+double x, y, sum;
 
 /*--------------------------------------------------------------------------------------------------
 FUNCTION:           main()
@@ -47,7 +49,8 @@ int main()
 
     char choice;                    // Declares variable for menu selections
 
-    while(true)                     // Open while loop to keep the program running
+    bool goodbye = false;           // Flag allows loop to be closed when switched to true
+    while(!goodbye)                 // Open while loop to allow multiple uses without restarting
     {
         choice = GetChoice();       // Calls function GetChoice to get user's selection
         switch (choice)
@@ -56,6 +59,7 @@ int main()
                 Add2Numbers();      // If choice == a, call Add2Numbers function
                 break;
             case 'q':
+                goodbye = true;
                 break;
         }
     }                              
@@ -117,20 +121,29 @@ char GetChoice()
 }
 
 /*--------------------------------------------------------------------------------------------------
-FUNCTION:           Add2Numbers()
-DESCRIPTION:        Addition function
+FUNCTION:           getXandY()
+DESCRIPTION:        Gets user input for math functions
 RETURNS:            N/A
 NOTES:              Inputting characters for x and/or y results in an infinite scroll
 --------------------------------------------------------------------------------------------------*/
-void Add2Numbers()
+void getXandY()
 {
-    double x, y, sum;
-
-    cout << "This program will add x and y... " << endl;
-    cout << endl << endl;
     cout << "Please input x and y: ";
     cin >> x;       // <!> BUG <!>
-    cin >> y;       // Inputting characters rather than numbers results in an infinite scroll    
+    cin >> y;       // Inputting characters rather than numbers results in an infinite scroll
+}
+
+/*--------------------------------------------------------------------------------------------------
+FUNCTION:           Add2Numbers()
+DESCRIPTION:        Addition function
+RETURNS:            N/A
+NOTES:              
+--------------------------------------------------------------------------------------------------*/
+void Add2Numbers()
+{
+    cout << "This program will add x and y... " << endl;
+    cout << endl << endl;
+    getXandY();
 
     sum = x + y;
 
