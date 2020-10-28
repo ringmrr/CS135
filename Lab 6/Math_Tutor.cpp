449f8373b677bfeb13e459ccf97d4ef36cd5d60d
Michael Ring <ringmrr@gmail.com>
2020-10-27 23:34:31 -0700

    MT1 0.4
    RNG works, but no result is displayed

--- Lab 6/Math_Tutor.cpp
+++ Lab 6/Math_Tutor.cpp
@@ -7,28 +7,23 @@ NOTES:
 MODIFICATION HISTORY:
 Author              Date            Version
 ---------------     ----------      ---------------
-Michael Ring        2020-10-26      0.1 - Copied from template
-Michael Ring        2020-10-26      0.2 - Set up while loop in main to call new menu() function
-Michael Ring        2020-10-26      0.3 - Created menu output and int input/return
-Michael Ring        2020-10-26      0.4 - Added function prototypes and respective cases
-Michael Ring        2020-10-26      0.5 - 
+Michael Ring        2020-10-26      0.1 - Declared variables and formatted output
+Michael Ring        2020-10-26      0.2 - Assign rand() to x and y
+                                            > Assigns same values each time the program is run.
+Michael Ring        2020-10-27      0.3 - Added if statement to set constraints for x and y
+Michael Ring        2020-10-27      0.4 - Added srand and way to generate seed
+                                            > Solution found in Starting out with C++ p.129
 --------------------------------------------------------------------------------------------------*/
 
 // PREPROCESSOR DIRECTIVES
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <cstdlib>
+#include <ctime>
 
 // NAMESPACE
 using namespace std;
-
-// FUNCTION PROTOTYPES
-int menu();
-
-void add();
-void subtract();
-void multiply();
-void divide();
 
 // NAMED CONSTANTS
 const string PROGRAMMER_NAME = "Michael Ring";
@@ -39,119 +34,29 @@ DESCRIPTION:        Say what this function does
 RETURNS:            0
 NOTES:              Optional notes on this function
 --------------------------------------------------------------------------------------------------*/
-
 int main()
 {
-    bool goodbye = false;
-    while(!goodbye)
-    {
-        int choice = menu();
-        cout << "chose " << choice << endl;
-        switch (choice)
-        {
-            case 1:
-                add();
-                break;
-            case 2:
-                subtract();
-                break;
-            case 3:
-                multiply();
-                break;
-            case 4:
-                divide();
-                break;
-            case 5:
-                goodbye = true;
-                break; // unnecessary
-        }
-    }
+    int x, y;
 
-    cout << "Programmed by: " << PROGRAMMER_NAME << " -- ";
-    cout << __DATE__ << " " __TIME__ << endl;
-    cout << endl;
-
-    system("pause");
-    return 0;
-}
-
-/*--------------------------------------------------------------------------------------------------
-FUNCTION:           menu()
-DESCRIPTION:        
-RETURNS:            choice
-NOTES:              No comments necessary -- pretty self explanatory
---------------------------------------------------------------------------------------------------*/
-int menu()
-{
-    cout << "        Math Tutor Menu"         << endl;
-    cout << "-------------------------------" << endl;
-    cout << "1. Addition problem"             << endl;
-    cout << "2. Subtraction problem"          << endl;
-    cout << "3. Multiplication problem"       << endl;
-    cout << "4. Diision problem"              << endl;
-    cout << "5. Quit this program"            << endl;
-    cout << "-------------------------------" << endl;
-    cout << "Enter your choice (1-5): ";
-
-    int choice;
+    unsigned seed = time(0);    // Gets the system time
+    srand(seed);                // Seeds RNG
 
     bool valid = false;
     while (!valid)
     {
-        cin >> choice;
-        cin.clear();
-        if (cin.good())
-        {
-            if (choice <= 5 && choice >= 0)
-                return choice;            
-        }
-        else
-            cout << "ERROR: Invalid input. Please try again.";
+        x = rand();             // Assigns random values
+        y = rand();             // to x and y
+
+        // Checks that x is a 3 digit number, and that y is a 2 or 3 digit number less than x.
+        if (x >= 100 && x <= 999 && y >= 10 && y < x)
+            valid = true;
     }
-    cin >> choice;
-    return choice;
-}   
 
-/*--------------------------------------------------------------------------------------------------
-FUNCTION:           add()
-DESCRIPTION:        
-RETURNS:            void
-NOTES:
---------------------------------------------------------------------------------------------------*/
-void add()
-{
-    //P129 (159)
-}
+    cout << setw(5) << right << x << endl;
+    cout << "+" << setw(4) << right << y << endl;
+    cout << "-----" << endl;
 
-/*--------------------------------------------------------------------------------------------------
-FUNCTION:           subtract()
-DESCRIPTION:        
-RETURNS:            void
-NOTES:
---------------------------------------------------------------------------------------------------*/
-void subtract()
-{
-
-}
-
-/*--------------------------------------------------------------------------------------------------
-FUNCTION:           multiply()
-DESCRIPTION:        
-RETURNS:            void
-NOTES:
---------------------------------------------------------------------------------------------------*/
-void multiply()
-{
-
-}
-
-/*--------------------------------------------------------------------------------------------------
-FUNCTION:           divide()
-DESCRIPTION:        
-RETURNS:            void
-NOTES:
---------------------------------------------------------------------------------------------------*/
-void divide()
-{
-
+    cout << "Programmed by: " << PROGRAMMER_NAME << " -- " << __DATE__ << " " __TIME__ << endl;
+    system("pause");
+    return 0;
 }
