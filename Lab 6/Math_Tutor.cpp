4372a5ebee2e848f0a6e585625677567a1063508
Michael Ring <ringmrr@gmail.com>
2020-10-27 23:42:29 -0700

    1.0
    Pauses and prints result

--- Lab 6/Math_Tutor.cpp
+++ Lab 6/Math_Tutor.cpp
@@ -13,6 +13,7 @@ Michael Ring        2020-10-26      0.2 - Assign rand() to x and y
 Michael Ring        2020-10-27      0.3 - Added if statement to set constraints for x and y
 Michael Ring        2020-10-27      0.4 - Added srand and way to generate seed
                                             > Solution found in Starting out with C++ p.129
+Michael Ring        2020-10-27      1.0 - Pauses and prints result
 --------------------------------------------------------------------------------------------------*/
 
 // PREPROCESSOR DIRECTIVES
@@ -36,26 +37,43 @@ NOTES:              Optional notes on this function
 --------------------------------------------------------------------------------------------------*/
 int main()
 {
-    int x, y;
+    int x, y, result;
 
-    unsigned seed = time(0);    // Gets the system time
-    srand(seed);                // Seeds RNG
+    // Gets the system time to seed RNG
+    unsigned seed = time(0);    
+    srand(seed);               
 
+    // Loop stays open until x and y are in the proper range
     bool valid = false;
     while (!valid)
     {
-        x = rand();             // Assigns random values
-        y = rand();             // to x and y
+        // Assigns random values to x and y
+        x = rand();             
+        y = rand();             
 
         // Checks that x is a 3 digit number, and that y is a 2 or 3 digit number less than x.
         if (x >= 100 && x <= 999 && y >= 10 && y < x)
             valid = true;
     }
 
+    result = x + y;
+
+    // Print calculation without result
+    cout << setw(5) << right << x << endl;
+    cout << "+" << setw(4) << right << y << endl;
+    cout << "-----" << endl << endl;
+
+    // Pause for user input
+    cout << "Press Enter to see the answer . . ." << endl;
+    cin.ignore();
+
+    // Print calculation with result
     cout << setw(5) << right << x << endl;
     cout << "+" << setw(4) << right << y << endl;
     cout << "-----" << endl;
+    cout << setw(5) << right << result << endl << endl;
 
+    // Goodbye!
     cout << "Programmed by: " << PROGRAMMER_NAME << " -- " << __DATE__ << " " __TIME__ << endl;
     system("pause");
     return 0;
